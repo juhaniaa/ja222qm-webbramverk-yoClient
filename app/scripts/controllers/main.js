44c9cd07fede39ba4c2a99703469f78e8cecf7be
Juhani Aavanen <ja222qm@student.lnu.se>
2015-03-11 16:13:34 +0200

    Working on google map angular

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,10 +8,16 @@
  * Controller of the ja222qmWebbramverkYoClientApp
  */
 angular.module('hunterApp')
-  .controller('MainCtrl', function ($scope) {
+  .controller('MainCtrl', function ($scope, uiGmapGoogleMapApi) {
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
+
+    $scope.map = { center: { latitude: 45, longitude: -73 }, zoom: 8};
+
+    uiGmapGoogleMapApi.then(function(maps) {
+
+    });
   });
